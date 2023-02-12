ff8d40f215dc86c9510a4d6c517471e1c223775b
Radha743 <worldofnara777@gmail.com>
2023-02-12 14:48:28 +0530

    working

--- src/game.js
+++ src/game.js
@@ -6,8 +6,19 @@ export default class Game {
   constructor(gameContainer) {
     this.gameContainer = gameContainer
     this.score = 0
-  }
+    this.modal = document.querySelector("[data-rules-modal]")
+    this.modelOpenBtn = document.querySelector("[data-open-rules]")
 
+    this.modal.addEventListener("click", (e) => {
+      if (e.target.classList.toString().match(/(?:close-btn|rules)/g)) {
+        this.modal.classList.add("hidden")
+      }
+    })
+
+    this.modelOpenBtn.addEventListener("click", () => {
+      this.modal.classList.remove("hidden")
+    })
+  }
   reset() {
     this.score = 0
   }
